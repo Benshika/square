20b32c9f5e3e8f463b0f33b02d23edecc9af6afa
Benshika <119404251+Benshika@users.noreply.github.com>
2023-12-31 10:51:22 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.speed(0)
+turtle.bgcolor("black")
+for i in range(5):
+    for colours in ["red","white","yellow","blue","cyan","pink"]:
+        turtle.color(colours)
+        turtle.pensize(3)
+        turtle.left(12)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
